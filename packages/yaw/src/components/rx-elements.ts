22075594011f45c3a59762eeeba6325d699fcb7e
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-04-19 13:32:16 +0100

    FIX: fixing default styles

--- packages/yaw/src/components/rx-elements.ts
+++ packages/yaw/src/components/rx-elements.ts
@@ -1,4 +1,4 @@
-import { htmlTags, mirrorDisplay } from 'yaw-common';
+import { htmlTags, mirrorStyles } from 'yaw-common';
 import { RxElementBase } from '../rx-element.js';
 import { Component } from '../component.js';
 
@@ -9,11 +9,11 @@ export const mirrorCtors = new Set<Function>();
 export const registerHtmlMirrors = (): void => {
     for (const tag of htmlTags) {
         const selector = `rx-${tag}`;
-        const display = mirrorDisplay.get(tag);
+        const style = mirrorStyles.get(tag);
         const ctor = class extends RxElementBase {};
         mirrorCtors.add(ctor);
-        const options = display !== undefined
-            ? { selector, styles: `${selector}{display:${display}}` }
+        const options = style !== undefined
+            ? { selector, styles: `${selector}{${style}}` }
             : { selector };
         Component(options)(ctor);
     }
