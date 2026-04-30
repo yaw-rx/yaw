cebc38ddc753c137ecac833faf37f256ac064e64
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-04-30 15:14:43 +0100

    CHORE: working on tree shaking rxjs

--- packages/yaw-example/vite.config.ts
+++ packages/yaw-example/vite.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
     root: '.',
     plugins: [vitePlugin()],
     esbuild: { target: 'es2022' },
+    resolve: {
+        conditions: ['es2015', 'import'],
+    },
     server: {
         port: 5176,
     },
